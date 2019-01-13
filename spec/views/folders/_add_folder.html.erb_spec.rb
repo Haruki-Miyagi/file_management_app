7dabd9acfbd1a7691f9f383d6ff7cf6fb215f929
Haruki Miyagi <haruki.miyagi1112@gmail.com>
2019-01-14 01:29:07 +0900

    Add folders-view spec

--- spec/views/folders/_add_folder.html.erb_spec.rb
+++ spec/views/folders/_add_folder.html.erb_spec.rb
@@ -41,36 +41,4 @@ RSpec.describe 'folders/index.html.erb', type: :view do
       end
     end
   end
-
-  context 'テーブルヘッダ' do
-    it 'ファイル名があること' do
-      assert_select 'table.table' do
-        assert_select 'thead th', text: 'ファイル名', count: 1
-      end
-    end
-
-    it '備考があること' do
-      assert_select 'table.table' do
-        assert_select 'thead th', text: '備考', count: 1
-      end
-    end
-  end
-
-  context 'テーブル本体' do
-    it 'リンク付きのファイルのテキストがある表示してあること' do
-      resources.each do |resource|
-        assert_select 'table.table' do
-          assert_select 'tbody td a[href=?]', folder_path(resource), text: resource.name
-        end
-      end
-    end
-
-    it '備考のテキストがある表示してあること' do
-      resources.each do |resource|
-        assert_select 'table.table' do
-          assert_select 'tbody td', text: resource.description
-        end
-      end
-    end
-  end
 end

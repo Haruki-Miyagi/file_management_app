ce4bf6da008f3c983ea0c5ea8106d7528942e193
Haruki Miyagi <haruki.miyagi1112@gmail.com>
2019-01-17 23:45:11 +0900

    Add rspec to documentカラム

---
+++ spec/models/document_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Document, type: :model do
+  describe 'column' do
+    it { is_expected.to have_db_column(:file_name) }
+    it { is_expected.to have_db_column(:uploaded_file) }
+    it { is_expected.to have_db_column(:description) }
+    it { is_expected.to have_db_column(:user_id) }
+    it { is_expected.to have_db_column(:room_id) }
+    it { is_expected.to have_db_column(:created_at) }
+    it { is_expected.to have_db_column(:updated_at) }
+  end
+
+  describe 'validation' do
+    it { is_expected.to validate_presence_of(:uploaded_file) }
+  end
+
+  describe 'association' do
+    it { is_expected.to belong_to(:user).class_name('User') }
+    it { is_expected.to belong_to(:room).class_name('Room') }
+  end
+end

312c30b45fc3598ace444a173598b53c9cf14de6
Haruki Miyagi <haruki.miyagi1112@gmail.com>
2019-01-27 13:57:58 +0900

    Fix ストロングパラメータ to documents_controller.rb

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -4,7 +4,8 @@ class DocumentsController < ApplicationController
   private
 
   def resource_params
-    UploadFileService.new(params[:document]).define_params
+    documents_params = params.require(:document).permit(:file_name, :file_date, :description, :user_id, :room_id)
+    UploadFileService.new(documents_params).define_params
   end
 
   def new_resource
--- app/services/upload_file_service.rb
+++ app/services/upload_file_service.rb
@@ -2,7 +2,7 @@ class UploadFileService
   def initialize(params = {})
     @file_name = change_file_name(params[:file_name], params[:file_date])
     @uploaded_file = params[:file_date]
-    @description = params[:description] || nil
+    @description = params[:description]
     @user_id = params[:user_id]
     @room_id = params[:room_id]
   end
@@ -20,10 +20,12 @@ class UploadFileService
   private
 
   def change_file_name(file_name, uploaded_file)
-    if file_name.present? && uploaded_file.present?
+    return nil if uploaded_file.blank?
+
+    if file_name.present?
       extname = File.extname(uploaded_file.original_filename)
       file_name + extname
-    elsif file_name.blank? && uploaded_file.present?
+    else
       uploaded_file.original_filename
     end
   end

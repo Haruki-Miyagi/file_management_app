781dd6c5c4c3e4fbf9ec5d63950e29c922b2cab4
Haruki Miyagi <haruki.miyagi1112@gmail.com>
2019-02-01 21:31:01 +0900

    Add spec to rooms/show.html.erb_spec.rb

--- spec/views/rooms/show.html.erb_spec.rb
+++ spec/views/rooms/show.html.erb_spec.rb
@@ -118,6 +118,18 @@ RSpec.describe 'rooms/show.html.erb', type: :view do
         assert_select 'thead th', text: '備考', count: 1
       end
     end
+
+    it '編集があること' do
+      assert_select 'table.table' do
+        assert_select 'thead th.text-center', text: '編集', count: 1
+      end
+    end
+
+    it '削除があること' do
+      assert_select 'table.table' do
+        assert_select 'thead th.text-center', text: '削除', count: 1
+      end
+    end
   end
 
   context 'テーブル本体' do
@@ -133,6 +145,35 @@ RSpec.describe 'rooms/show.html.erb', type: :view do
       documents.each do |document|
         assert_select 'table.table' do
           assert_select 'tbody td', text: document.description
+        end
+      end
+    end
+
+    context '編集' do
+      it 'リンク付きの編集アイコンがあること' do
+        documents.each do |document|
+          assert_select 'table.table' do
+            assert_select 'tbody td a[href=?]', edit_room_document_path(resource, document) do
+              assert_select 'i[class=?]', 'glyphicon glyphicon-edit'
+            end
+          end
+        end
+      end
+
+      it 'ポップアップメッセージがあること' do
+        assert_select 'table.table td' do
+          assert_select 'a[title=?]', '編集するにはクリックしてください'
+          assert_select 'a[data-toggle=?]', 'tooltip'
+        end
+      end
+    end
+
+    it 'リンク付きの削除アイコンがあること' do
+      documents.each do |document|
+        assert_select 'table.table' do
+          assert_select 'tbody td a[href=?][data-method="delete"]', room_document_path(resource, document) do
+            assert_select 'i[class=?]', 'glyphicon glyphicon-trash'
+          end
         end
       end
     end
